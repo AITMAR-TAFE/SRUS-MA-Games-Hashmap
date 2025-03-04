4fec6d87a27d27aecd0d99858dfe220cc0462ff8
Martina Ait <20114816@tafe.wa.edu.au>
2025-03-04 14:09:50 +0800

    Step 6. Added tail functionality

--- app/player_list.py
+++ app/player_list.py
@@ -4,6 +4,7 @@ from app.player_node import PlayerNode
 class PlayerList(object):
     def __init__(self, values=None):
         self._head = None
+        self._tail = None
         self._len = 0
         if values is not None:
             for item in values:
@@ -14,14 +15,20 @@ class PlayerList(object):
 
     def push(self, value):
         new_node = PlayerNode(value)
-        if self._head is None:
+
+        if self.is_empty():
             self._head = new_node
+            self._tail = new_node
 
         else:
+            new_node.next_player = self._head
+            self._head.prev_node = new_node
+            self._head = new_node
+
             current_node = self._head
             while current_node.next_node is not None:
                 current_node = current_node.next_node
-            current_node.next_node = new_node
+                self._tail = current_node
 
         self._len += 1
 
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from app.player_list import PlayerList
+from app.player import Player
 
 class TestPlayerList(unittest.TestCase):
     def test_player_list_empty(self):
@@ -10,3 +11,11 @@ class TestPlayerList(unittest.TestCase):
     def test_player_list(self):
         test_player_list = PlayerList([1,2,3])
         self.assertEqual(len(test_player_list),3)
+
+    def test_player_list_head_and_tail(self):
+        player_list = PlayerList()
+        player_list.push('Player1')
+        player_list.push('Player2')
+        player_list.push('Player3')
+        self.assertEqual(player_list._head._player, 'Player3')
+        self.assertEqual(player_list._tail._player, 'Player1')
