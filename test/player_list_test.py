26b3eff356412747cf5557eed688cbf5ea0e4787
Martina at Home <20114816@tafe.wa.edu.au>
2025-03-07 10:29:48 +0800

    Added docstring to player_list_test

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -6,7 +6,11 @@ from app.player_list import PlayerList
 from app.player import Player
 
 
-class Capturing(list):         # ref https://gist.github.com/russhughes/5afb0dca6e6b73d5c10d707334b06615
+class Capturing(list):
+    """
+    Context manager to capture printed output during tests.
+    Reference https://gist.github.com/russhughes/5afb0dca6e6b73d5c10d707334b06615
+    """
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = StringIO()
@@ -22,6 +26,7 @@ class Capturing(list):         # ref https://gist.github.com/russhughes/5afb0dca
 class TestPlayerList(unittest.TestCase):
 
     def setUp(self):
+        """Set up a test player list with sample players."""
         self.player_list = PlayerList()
         players = [
             Player(unique_id=1, player_name="Player1"),
@@ -34,36 +39,48 @@ class TestPlayerList(unittest.TestCase):
             self.player_list.push(player)
 
     def test_player_list_empty(self):
+        """Test if a new list is empty."""
         empty_list = PlayerList()
         self.assertTrue(empty_list.is_empty())
 
     def test_player_list(self):
+        """Test the length of the player list."""
         self.assertEqual(len(self.player_list), 5)
 
     def test_player_list_head_and_tail(self):
+        """Test if head and tail are set correctly after initialization."""
         self.assertEqual(self.player_list._head._player.name(), 'Player5')
         self.assertEqual(self.player_list._tail._player.name(), 'Player1')
 
     def test_player_list_push_tail(self):
+        """Test the push_tail method to add a player to the end of the list."""
         self.player_list.push_tail(Player(unique_id=6, player_name="PlayerPUSH"))
         self.assertEqual(self.player_list._tail._player.name(), 'PlayerPUSH')
         self.assertEqual(self.player_list._head._player.name(), 'Player5')
+        self.assertEqual(len(self.player_list), 6)
 
     def test_player_list_delete_head(self):
+        """Test deleting the head player from the list."""
         self.player_list.delete_head()
         self.assertEqual(self.player_list._head._player.name(), 'Player4')
+        self.assertEqual(len(self.player_list), 4)
 
     def test_player_list_delete_tail(self):
+        """Test deleting the tail player from the list."""
         self.player_list.delete_tail()
         self.assertEqual(self.player_list._tail._player.name(), 'Player2')
+        self.assertEqual(len(self.player_list), 4)
 
     def test_player_list_delete_key(self):
+        """Test deleting a player by unique key."""
         self.player_list.delete_key(4)
         self.assertEqual(self.player_list._head._player.name(), "Player5")
         self.assertEqual(self.player_list._tail._player.name(), "Player1")
         self.assertEqual(self.player_list._head.next_node._player.name(), "Player3")
+        self.assertEqual(len(self.player_list), 4)
 
     def test_player_list_display(self):
+        """Test displaying the player list."""
         with Capturing() as output:
             self.player_list.display(True)
 
