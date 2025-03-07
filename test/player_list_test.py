07e29dc0e759253613e035050cfbf9885d9edc8c
Martina at Home <20114816@tafe.wa.edu.au>
2025-03-07 09:45:53 +0800

    Cleaned up test file from repetition

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -20,74 +20,52 @@ class Capturing(list):         # ref https://gist.github.com/russhughes/5afb0dca
 
 
 class TestPlayerList(unittest.TestCase):
+
+    def setUp(self):
+        self.player_list = PlayerList()
+        players = [
+            Player(unique_id=1, player_name="Player1"),
+            Player(unique_id=2, player_name="Player2"),
+            Player(unique_id=3, player_name="Player3"),
+            Player(unique_id=4, player_name="Player4"),
+            Player(unique_id=5, player_name="Player5")
+        ]
+        for player in players:
+            self.player_list.push(player)
+
     def test_player_list_empty(self):
-        test_player_list = PlayerList()
-        self.assertEqual(test_player_list.is_empty(), True)
+        empty_list = PlayerList()
+        self.assertTrue(empty_list.is_empty())
 
     def test_player_list(self):
-        test_player_list = PlayerList()
-        test_player_list.push(Player(unique_id=1, player_name="Player1"))
-        test_player_list.push(Player(unique_id=2, player_name="Player2"))
-        test_player_list.push(Player(unique_id=3, player_name="Player3"))
-        self.assertEqual(len(test_player_list), 3)
+        self.assertEqual(len(self.player_list), 5)
 
     def test_player_list_head_and_tail(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        self.assertEqual(player_list._head._player.name(), 'Player3')
-        self.assertEqual(player_list._tail._player.name(), 'Player1')
+        self.assertEqual(self.player_list._head._player.name(), 'Player5')
+        self.assertEqual(self.player_list._tail._player.name(), 'Player1')
 
     def test_player_list_push_tail(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        player_list.push_tail(Player(unique_id=4, player_name="PlayerPUSH"))
-        player_list.push(Player(unique_id=5, player_name="Player4"))
-        self.assertEqual(player_list._tail._player.name(), 'PlayerPUSH')
-        self.assertEqual(player_list._head._player.name(), 'Player4')
+        self.player_list.push_tail(Player(unique_id=6, player_name="PlayerPUSH"))
+        self.assertEqual(self.player_list._tail._player.name(), 'PlayerPUSH')
+        self.assertEqual(self.player_list._head._player.name(), 'Player5')
 
     def test_player_list_delete_head(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        player_list.delete_head()
-        self.assertEqual(player_list._head._player.name(), 'Player2')
+        self.player_list.delete_head()
+        self.assertEqual(self.player_list._head._player.name(), 'Player4')
 
     def test_player_list_delete_tail(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        player_list.delete_tail()
-        self.assertEqual(player_list._tail._player.name(), 'Player2')
+        self.player_list.delete_tail()
+        self.assertEqual(self.player_list._tail._player.name(), 'Player2')
 
     def test_player_list_delete_key(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        player_list.push(Player(unique_id=4, player_name="Player4"))
-        player_list.push(Player(unique_id=5, player_name="Player5"))
-        player_list.delete_key(4)
-        self.assertEqual(player_list._head._player.name(),"Player5")
-        self.assertEqual(player_list._tail._player.name(),"Player1")
-        self.assertEqual(player_list._head.next_node._player.name(),"Player3")
+        self.player_list.delete_key(4)
+        self.assertEqual(self.player_list._head._player.name(), "Player5")
+        self.assertEqual(self.player_list._tail._player.name(), "Player1")
+        self.assertEqual(self.player_list._head.next_node._player.name(), "Player3")
 
     def test_player_list_display(self):
-        player_list = PlayerList()
-        player_list.push(Player(unique_id=1, player_name="Player1"))
-        player_list.push(Player(unique_id=2, player_name="Player2"))
-        player_list.push(Player(unique_id=3, player_name="Player3"))
-        player_list.push(Player(unique_id=4, player_name="Player4"))
-        player_list.push(Player(unique_id=5, player_name="Player5"))
-
-        # Capture the printed output using the Capturing class previously made
         with Capturing() as output:
-            player_list.display(True)
+            self.player_list.display(True)
 
         expected_output = [
             "Player ID: 5, Name: Player5",
