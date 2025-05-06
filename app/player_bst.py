6c1066142cb46a62b11775a5a3ab3ed40a18f234
Martina Ait <20114816@tafe.wa.edu.au>
2025-05-06 14:51:01 +0800

    Created balanced_bst function, starting to create tests

--- app/player_bst.py
+++ app/player_bst.py
@@ -51,7 +51,17 @@ class PlayerBST:
                 node = node.right
 
     def balanced_bst(self, sorted_list):
-        return 1
+        def build_recursively(my_list):
+            if len(my_list) == 0: #If the list is empty
+                return None
+            middle_element = len(my_list) // 2
+            node = PlayerBNode(my_list[middle_element])
+            node.left = build_recursively(my_list[:middle_element])
+            node.right = build_recursively(my_list[middle_element + 1:])
+            print("The middle:", node," The left:", node.left, " The right:", node.right)
+            return node
+
+        self.root = build_recursively(sorted_list)
 
     def create_sorted_list(self, node, in_order_list=None):
         if in_order_list is None:
@@ -79,11 +89,16 @@ player2 = Player("ID2", "Bob", 150)
 player3 = Player("ID3", "Charlie", 200)
 player4 = Player("ID4", "Fiona", 90)
 player5 = Player("ID5", "Elena", 180)
+player6 = Player("ID6", "Aadila", 120)  # Should be before Alice
+player7 = Player("ID7", "Xavier", 300)
 
 tree.insert(player4)
 tree.insert(player2)
 tree.insert(player5)
 tree.insert(player3)
 tree.insert(player1)
+tree.insert(player6)
+tree.insert(player7)
 
-print(tree.create_sorted_list(tree.root))
+sorted_players = tree.create_sorted_list(tree.root)
+tree.balanced_bst(sorted_players)
